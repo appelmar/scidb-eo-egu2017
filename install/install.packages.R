d06935f63749bacaaa2b27eaa5cc791b901dc340
Marius Appel <marius.appel@uni-muenster.de>
2017-04-28 14:57:33 +0200

    minminor fixes

--- install/install.packages.R
+++ install/install.packages.R
@@ -1,4 +1,4 @@
 install.packages(c("devtools"), repos="http://cran.rstudio.com/")
 install.packages(c("bfast","raster","sp","gdalUtils", "rgdal", "plyr","xts"), repos="http://cran.rstudio.com/")
-devtools::install_github("Paradigm4/SciDBR")
-devtools::install_github("appelmar/scidbst", ref="dev")
+devtools::install_github("Paradigm4/SciDBR", ref="419a8968149fca9a72fbd55a3dd8d4e8897181a3") 
+devtools::install_github("flahn/scidbst", ref="dev")
